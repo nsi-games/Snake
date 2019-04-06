e3de910ac60cacebe13660b536680783a26017be
NSI Game Teachers <nsigameteachers@gmail.com>
2019-04-07 01:06:54 +1100

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Snake
+{
+    public class GameManager : MonoBehaviour
+    {
+        public int score = 0;
+
+        [Header("UI")]
+        public Text scoreText;
+        
+        public static GameManager Instance = null;
+        public delegate void SpawnCallback();
+        public SpawnCallback onSpawn;
+
+        public delegate void ScoreCallback(int score);
+        public ScoreCallback onScoreAdded;
+
+        // Keep record of the score
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+        }
+        
+        // Access this function to spawn whatever subscribed to spawn callback
+        public void Spawn()
+        {
+            // If there are subscribed functions
+            if(onSpawn != null)
+            {
+                // Invoke them
+                onSpawn.Invoke();
+            }
+        }
+
+        public void AddScore(int scoreToAdd)
+        {
+            score += scoreToAdd;
+            scoreText.text = "Score: " + score.ToString();
+
+            // Are functions subscribed to onScoreAdded?
+            if(onScoreAdded != null)
+            {
+                // Invoke all subscribed
+                onScoreAdded.Invoke(score);
+            }
+        }
+
+        public void ResetGame()
+        {
+            Scene currentScene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(currentScene.name);
+        }
+    }
+}
---
+++ Assets/Scripts/Other/DontDestroyOnLoad.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Snake
+{
+    public class DontDestroyOnLoad : MonoBehaviour
+    {
+        // Use this for initialization
+        void Start()
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
